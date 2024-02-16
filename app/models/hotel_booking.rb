abadb472be7ceea5c33800d47bff00e464a7050b
surendra.goud750 <surendra.goud750@gmail.com>
2024-02-16 22:26:48 +0530

    Add validation

--- app/models/hotel_booking.rb
+++ app/models/hotel_booking.rb
@@ -4,5 +4,6 @@ class HotelBooking < ApplicationRecord
 	belongs_to :hotel_room
     
     enum status: %i[available closed booked]
-
+    
+    validates :check_in_date, :check_out_date, :hotel_room_id, :hotel_id, :user_id, presence: true
 end
