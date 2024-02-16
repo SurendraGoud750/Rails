62265796db2687a079c2b2c87aec452500e37ea5
surendra.goud750 <surendra.goud750@gmail.com>
2024-02-16 22:29:00 +0530

    Add validation on check in and check out date

--- app/models/hotel_booking.rb
+++ app/models/hotel_booking.rb
@@ -6,4 +6,5 @@ class HotelBooking < ApplicationRecord
     enum status: %i[available closed booked]
     
     validates :check_in_date, :check_out_date, :hotel_room_id, :hotel_id, :user_id, presence: true
+    validates :check_out_date, presence: true, date: { after_or_equal_to:  :check_in_date}
 end
